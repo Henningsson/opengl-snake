d04f83387f0e3896deec1666c325aa6c6b02928c
Henningsson <henningssontimmy@gmail.com>
2014-03-27 13:50:26 +0100

    Mappar

---
+++ include/Object.h
@@ -0,0 +1,22 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <SFML/Window.hpp>
+#include "GL_utilities.h"
+#include "loadobj.h";
+
+
+typedef GLuint texture_t;
+typedef GLuint shader_t;
+
+class Object
+{
+ public:
+  
+
+ private:
+  mat4
+
+};
+
+#endif //OBJECT_H
